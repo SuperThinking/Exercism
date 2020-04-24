f355465d678e7763bb39d97bb20d4e6a560bfce1
SuperThinking <superdhawan@gmail.com>
2020-04-24 13:58:01 +0530

    protein_translation

---
+++ elixir/protein-translation/lib/protein_translation.ex
@@ -0,0 +1,70 @@
+defmodule ProteinTranslation do
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: {atom, list(String.t())}
+  def of_rna(rna) do
+    rec(rna, [])
+  end
+
+  def rec(rna, new_list) do
+    {a, z} = String.split_at(rna, 3)
+    x = of_codon(a)
+    case x do
+      {:ok, "STOP"} -> {:ok, new_list}
+      {:ok, codon} ->
+        rec(z, new_list++[codon])
+      {:error, _} -> {:error, "invalid RNA"}
+    end
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: {atom, String.t()}
+  def of_codon(codon) do
+    m = %{
+      "UGU" => "Cysteine",
+      "UGC" => "Cysteine",
+      "UUA" => "Leucine",
+      "UUG" => "Leucine",
+      "AUG" => "Methionine",
+      "UUU" => "Phenylalanine",
+      "UUC" => "Phenylalanine",
+      "UCU" => "Serine",
+      "UCC" => "Serine",
+      "UCA" => "Serine",
+      "UCG" => "Serine",
+      "UGG" => "Tryptophan",
+      "UAU" => "Tyrosine",
+      "UAC" => "Tyrosine",
+      "UAA" => "STOP",
+      "UAG" => "STOP",
+      "UGA" => "STOP",
+      "" => "STOP"
+    }
+    case Map.get(m, codon, :error) do
+      :error -> {:error, "invalid codon"}
+      _ -> {:ok, Map.get(m, codon, :error)}
+    end
+
+  end
+end
